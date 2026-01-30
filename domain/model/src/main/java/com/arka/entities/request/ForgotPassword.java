51df595e7abf7091413ed50674f258fc2261d5e7
Juan Sebastian Cabrera <jusekaa@hotmail.com>
2026-01-29 19:36:35 -0500

    proyecto de seguridad para el proyecto arka

--- domain/model/src/main/java/com/arka/entities/request/ForgotPassword.java
+++ domain/model/src/main/java/com/arka/entities/request/ForgotPassword.java
@@ -1,7 +1,5 @@
 package com.arka.entities.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ForgotPassword {
-
-    @NotBlank(message = "El email es obligartorio")
-    @Email(message = "El email ingreso no es valido")
     private String email;
 }
